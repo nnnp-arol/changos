b952d1216e7c061860795c89671a856226dd4988
nnnp-arol <arol.alonso@nonerds.com>
2023-05-27 15:42:29 -0300

    add juli profile

--- src/utils/profiles.ts
+++ src/utils/profiles.ts
@@ -1,9 +1,11 @@
 import prof1 from "../assets/images/jeanProfile.png";
 import prof2 from "../assets/images/arolProfile.png";
 import prof3 from "../assets/images/aguProfile.png";
+import prof4 from "../assets/images/juliProfile.png";
 
 const profile1 = { user: "jean", password: "12qwaszx", img: prof1 };
 const profile2 = { user: "arol", password: "12qwaszx", img: prof2 };
 const profile3 = { user: "agu", password: "12qwaszx", img: prof3 };
+const profile4 = { user: "juli", password: "qweasdzxc", img: prof4 };
 
 export const profiles = [profile1, profile2, profile3];
